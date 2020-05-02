40825e6a09787994f9df4f2b0c36546bb15a8941
Merlin <merlin.lefevre@telecomnancy.net>
2020-05-02 23:09:14 +0200

    Début mise en place phase 0a

---
+++ Assets/TrapsStore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapsStore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void onClickPlus(MapTrap mapTrap)
+    //{
+
+    //}
+
+    //void onClickMoins(MapTrap mapTrap)
+    //{
+
+    //}
+}

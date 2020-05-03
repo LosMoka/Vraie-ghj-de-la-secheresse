ffba2c86050e19199cb4548a88ffd7a71b70de0c
Merlin <merlin.lefevre@telecomnancy.net>
2020-05-03 09:52:25 +0200

    Pre-merge

---
+++ Assets/Model/ElementsStore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ModelToView;
+
+namespace Model
+{
+
+    public class ElementsStore : MonoBehaviour
+    {
+        public string NameText;
+        private Environment m_environment;
+        private EnvironmentStore m_environmentStore;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_environment = new Environment(100000);
+            m_environmentStore = new EnvironmentStore(m_environment);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void onClickPlus()
+        {
+            //bool isBuyable = m_environmentStore.buyEnvironmentPerk(gameObject);
+            NameText = gameObject.name;
+            Debug.LogError("onClickPlus ES" + " " + gameObject.name);
+        }
+
+        public void onClickMoins()
+        {
+            Debug.LogError("onClickMoins ES");
+        }
+    }
+
+}
---
+++ Assets/Model/TrapsStore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class TrapsStore : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void onClickPlus()
+        {
+            Debug.LogError("onClickPlus TS");
+        }
+
+        public void onClickMoins()
+        {
+            Debug.LogError("onClickMoins TS");
+        }
+    }
+}
